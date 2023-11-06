6b9fe2551cfb6984920145e7e22762a17f693e42
Gabriel Urena <54905586+GabeUrena@users.noreply.github.com>
2023-11-06 10:35:31 -0500

    Working with functions (Parameters and arguments)

---
+++ Day7/Parameters.py
@@ -0,0 +1,27 @@
+def greeting(first_name, last_name):  # Define functions with def
+    print(f"Hi {first_name} {last_name}!")
+    print("Welcome to this program")
+    farwell(first_name)
+
+def farwell(first_name):
+    print(f"Goodbye {first_name}.")
+
+print("Start")
+greeting("Gabriel","Urena")
+print("Finish")
+
+# Parameters are the variables attached to the functions like
+# def greetings(name): name is the parameter
+# An argument is the thing we assign to that for instance
+# greeting("Gabriel") the string "Gabriel" is the argument
+
+#Key word arguments
+
+def hello(fname, lname):
+    print(f"{fname}: Hello there my name is {fname} {lname}!")
+    print("It's nice to meet you.")
+    
+    #using keyword arguments helps us in some cases, they let us assign arguemtns without worrying about position
+hello(lname="Smith", fname="John")
+    #positional arguement
+hello("John", "Smith")
